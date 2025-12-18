9307a85e5c41931ce80cdeb1d993c77fa64b509b
saintreborn <richardsonreuben78@gmail.com>
2025-12-18 20:13:43 +0300

    user registration and login setup

--- libs/core/src/user-id-and-password.model.ts
+++
@@ -1,4 +0,0 @@
-export type UserIdAndPassword = {
-    readonly user_id: string;
-    readonly password: string;
-}
--- libs/core/src/user-password-change.model.ts
+++
@@ -1,4 +0,0 @@
-export type UserPasswordChange = {
-    password_change_id?: string;
-    user_id?: string;
-}
